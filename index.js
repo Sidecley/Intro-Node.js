6593af7f23ca5295b066de5f752729fd2723895e
Sidecley Batista Inacio <65920956+Sidecley@users.noreply.github.com>
2020-06-12 13:49:53 -0300

    teste de conexão

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port,() => console.log('API rodando na porta: ' + port))
+
+
 console.log("Node rodando normalmente");
