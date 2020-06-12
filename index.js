163e443dd585def0ccf3e6b9a44404189f3fa789
Sidecley Batista Inacio <65920956+Sidecley@users.noreply.github.com>
2020-06-12 18:00:37 -0300

    aulas 1 e 2 atualizadas

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const userRoute = require('./routes/userRoute')
+
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Olá Mundo pelo Express!'))
+
 app.listen(port,() => console.log('API rodando na porta: ' + port))
 
-
-console.log("Node rodando normalmente");
